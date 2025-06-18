cd101c24d55e412e728d24f2bca52065de166c92
srimanikantaBattu <srimanikantabattu04@gmail.com>
2025-06-18 12:05:24 +0530

    Added all the working code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import GeminiChat from "./components/Gemini"
 
 function App() {
   return (
-    <div className="min-h-screen bg-zinc-950 flex items-center justify-center p-4">
-      <div className="bg-zinc-900 rounded-lg shadow-2xl w-full max-w-6xl flex flex-col h-[92vh] overflow-hidden border border-zinc-800">
+    <div>
+      <div>
         <GeminiChat />
       </div>
     </div>
